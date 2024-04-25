204870d930fb215d2a10e2f8ff03db73a0ccb357
Mahnaz Meem <mahnazsarkermeem@gmail.com>
2024-04-25 00:31:22 -0400

    Update logo

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -5,12 +5,12 @@ st.set_page_config(layout="wide")
 
 markdown = """
 A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+<https://github.com/Mahnaz-Meem/streamlit>
 """
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
+logo = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWlieTY0ZTh1cDJtdDJsY3hnNms1b2x5bGJjN3dtOTg0a3d2bHNjdSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UOdoMz3baCENO/giphy.gif"
 st.sidebar.image(logo)
 
 st.title("Split-panel Map")
